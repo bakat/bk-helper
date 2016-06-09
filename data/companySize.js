ca173f20680ef7ef389a8920042ccea2c79a0d73
Jepri Sinaga <therexxar@gmail.com>
2016-06-09 14:44:49 +0700

    add company size

---
+++ data/companySize.js
@@ -0,0 +1,27 @@
+'use strict'
+
+module.exports = [{
+    name: 'Micro',
+    size: '0-10',
+    value: 'A'
+ }, {
+    name: 'Small',
+    size: '11-50',
+    value: 'A'
+ }, {
+    name: 'Mid-Medium',
+    size: '51-250',
+    value: 'B'
+ }, {
+    name: 'Medium',
+    size: '251-500',
+    value: 'D'
+ }, {
+    name: 'Big',
+    size: '501-1000',
+    value: 'E'
+ }, {
+    name: 'Growing',
+    size: '>1000',
+    value: 'F'
+ }]
