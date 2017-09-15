d7b44a4f8a65877214469f6d2938fdf6e7c4f4ad
Hengki Sihombing <hengki.sihombing@gmail.com>
2017-09-15 12:50:55 +0700

    add data static for Free Job boards

---
+++ data/freeJobBoards.js
@@ -0,0 +1,194 @@
+module.exports = [
+  {
+    name: 'Urbanhire',
+    domain: 'urbanhire.com',
+    host: 'urbanhire',
+    provider: 'urbanhire',
+    logo: '//cdn.urbanhire.com/img/publisher/urbanhire.png',
+    description: 'Urbanhire is a single job market place that helps you find the perfect talents, Your job will be distributed directly to urbanhire.com within 250.000 Active jobseekers, make talent and offers the best opportunity for job seekers to get hired.'
+  },
+  {
+    name: 'Linkedin',
+    domain: 'linkedin.com',
+    host: 'linkedin',
+    provider: 'linkedin',
+    logo: '//cdn.urbanhire.com/img/publisher/linkedin.png',
+    description: 'Your job will be distributed to linkedin.com/jobs as limited listing and appear in their searc results.'
+  },
+  {
+    name: 'Kompaskarier',
+    domain: 'kompaskarier.com',
+    host: 'kompaskarier',
+    provider: 'kompaskarier',
+    logo: '//cdn.urbanhire.com/img/publisher/kompaskarier.png',
+    description: 'KompasKarier.com has become one of the leading online recruitment websites in Indonesia. Through computer technology and the Internet, KompasKarier.com help to connected with job seekers with a simple search.'
+  },
+  {
+    name: 'Indeed',
+    domain: 'indeed.com',
+    host: 'indeed',
+    provider: 'indeed',
+    logo: '//cdn.urbanhire.com/img/publisher/indeed.png',
+    description: 'As the world\'s #1 job site, with over 200 million unique visitors every month from over 60 different countries, Indeed has become the catalyst for putting the world to work. Indeed is intensely passionate about delivering the right fit for every hire. Indeed helps companies of all sizes hire the best talent and offers the best opportunity for job seekers to get hired.'
+  },
+  {
+    name: 'Glassdoor',
+    domain: 'glassdoor.com',
+    host: 'glassdoor',
+    provider: 'glassdoor.com',
+    logo: '//cdn.urbanhire.com/img/publisher/glassdoor.png',
+    description: 'Glassdoor holds a growing database of more than 8 million company reviews, CEO approval ratings, salary reports, interview reviews and questions, benefits reviews, office photos and more. Add to that millions of the latest jobs — no other site allows you to see which employers are hiring, what it\'s really like to work or interview there, and how much you could earn. Glassdoor is also available via its mobile app on iOS and Android platforms.'
+  },
+  {
+    name: 'Job-Like',
+    domain: 'job-like.com',
+    host: 'job-like',
+    provider: 'job-like',
+    logo: '//cdn.urbanhire.com/img/publisher/job-like.png',
+    description: 'Job-like is a job portal within 60,000 white-collar jobs in Indonesia, including Jakarta'
+  },
+  {
+    name: 'Jobplanet',
+    domain: 'id.jobplanet.com',
+    host: 'jobplanet',
+    provider: 'jobplanet',
+    logo: '//s3-ap-southeast-1.amazonaws.com/urbanhire/jobplanet-logo.png',
+    description: 'Jobplanet merupakan platform komunitas online untuk berbagi informasi seputar dunia kerja dan perusahaan. Jobplanet menampilkan review tentang budaya dan suasana kerja di perusahaan, informasi gaji, serta kisi-kisi pertanyaan dalam wawancara kerja.'
+  },
+  {
+    name: 'Jobtome',
+    domain: 'id.jobtome.com',
+    host: 'jobtome',
+    provider: 'jobtome',
+    logo: '//s3-ap-southeast-1.amazonaws.com/urbanhire/jobtome-logo.png',
+    description: 'Jobtome is one of the most important job search engines in the world. We help job seekers access millions of jobs gathered from a hundred thousand recruitment agency and employer sites. We help companies reach millions of job seekers via pay-per-click based job advertising services.'
+  },
+  {
+    name: 'Trovit',
+    domain: 'trovit.co.id',
+    host: 'trovit',
+    provider: 'trovit',
+    logo: '//cdn.urbanhire.com/img/publisher/trovit.png',
+    description: 'Trovit helps people find the most relevant ads published in thousands of classified websites, saving them the time it would take to surf all those pages individually. To make it even easier, users can set up their own personalized alerts for their searches and Trovit notifies them when there is a new ad that might be of interest.'
+  },
+  {
+    name: 'Jora',
+    domain: 'jora.com',
+    host: 'jora',
+    provider: 'id.jora.com',
+    logo: '//cdn.urbanhire.com/img/publisher/jora.png',
+    description: 'Jora Indonesia is a job information provider\'s website. display job from various portals lockers in Indonesia, including Jobstreet and JobsDB, company career pages, as well as recruitment agencies are seeking candidates for the various positions, so that job seekers can find a job without the need to open the locker sites one by one.'
+  },
+  {
+    name: 'Careerjet',
+    domain: 'careerjet.co.id',
+    host: 'careerjet',
+    provider: 'careerjet',
+    logo: '//cdn.urbanhire.com/img/publisher/careerjet.png',
+    description: 'Careerjet is a job search engine designed to make the process of finding a job on the internet easier for the user. It maps the huge selection of job offerings available on the internet in one extensive database by referencing job listings originating from job boards, recruitment agency websites and large specialist recruitment sites'
+  },
+  {
+    name: 'Jobrapido',
+    domain: 'id.jobrapido.com',
+    host: 'jobrapido',
+    provider: 'jobrapido',
+    logo: '//s3-ap-southeast-1.amazonaws.com/urbanhire/jobrapido-logo.png',
+    description: 'Jobrapido is the second largest global player in the growing job aggregator segment providing a one-stop platform for job-seekers to connect with employers. As industry frontrunner Jobrapido is shaping a new standard in online job searching; to revolutionize the way people get jobs. '
+  },
+  {
+    name: 'Jooble',
+    domain: 'id.jooble.org',
+    host: 'jooble',
+    provider: 'id.jooble.org',
+    logo: '//cdn.urbanhire.com/img/publisher/jooble.png',
+    description: 'Jooble display job postings indexed from various job boards, companies, websites, digital newspapers and recruitment agencies'
+  },
+  {
+    name: 'Mitula',
+    domain: 'mitula.co.id',
+    host: 'mitula',
+    provider: 'mitula.co.id',
+    logo: '//cdn.urbanhire.com/img/publisher/mitula.png',
+    description: 'Mitula is a vertical search engine for jobs classified ads. In one click you get free and detailed information about your search. mitula offers you thousands of ads from hundreds of our partner´s specialized portals.'
+  },
+  {
+    name: 'Edajobs',
+    domain: 'www.edajobs.com',
+    host: 'edajobs',
+    provider: 'edajobs',
+    logo: '//s3-ap-southeast-1.amazonaws.com/urbanhire/edajobs-logo.png',
+    description: 'EDAJOBS.COM membantu Anda untuk menemukan pekerjaan baru yang lebih mudah, sehingga Anda dapat memberikan resume Anda langsung ke perusahaan perekrutan.'
+  },
+  {
+    name: 'Klikkarir',
+    domain: 'www.klikkarir.com',
+    host: 'klikkarir',
+    provider: 'klikkarir',
+    logo: '//s3-ap-southeast-1.amazonaws.com/urbanhire/klikkarir-logo.png',
+    description: 'Portal lowongan kerja di Indonesia dengan lebih dari 25 ribu pencari kerja dan 5 ribu perusahaan. Buat resume/CV atau pasang iklan lowongan gratis!'
+  },
+  {
+    name: 'Cari.co',
+    domain: 'jobs.cari.co',
+    host: 'cari.co',
+    provider: 'cari.co',
+    logo: '//s3-ap-southeast-1.amazonaws.com/urbanhire/carico-logo.gif',
+    description: 'Mesin pencari mobil bekas, motor bekas, rumah dijual, lowongan kerja dan karir.'
+  },
+  {
+    name: 'Gigajob',
+    domain: 'gigajob.com',
+    host: 'gigajob',
+    provider: 'gigajob',
+    logo: '//cdn.urbanhire.com/img/publisher/gigajob.png',
+    description: 'Gigajob is a people-driven job portal. Gigajob brings employers and employees together - easily, quickly, and for free! Both job seekers and employers will benefit from our wide range of services at Gigajob'
+  },
+  {
+    name: 'Neuvoo',
+    domain: 'id.neuvoo.com',
+    host: 'neuvoo',
+    provider: 'neuvoo',
+    logo: '//cdn.urbanhire.com/img/publisher/neuvoo.png',
+    description: 'Pencarian lowongan kerja Anda mulai dari sini.'
+  },
+  {
+    name: 'Waa2',
+    domain: 'www.waa2.co.id',
+    host: 'waa2',
+    provider: 'waa2',
+    logo: '//cdn.urbanhire.com/img/publisher/waa2.png',
+    description: 'Mesin pencari tercepat di dunia, waa2 membawa jutaan rumah, mobil, dan tawaran pekerjaan untuk Anda gratis.'
+  },
+  {
+    name: 'Locanto',
+    domain: 'www.locanto.id',
+    host: 'locanto',
+    provider: 'locanto',
+    logo: '//cdn.urbanhire.com/img/publisher/locanto.png',
+    description: 'Temukan lebih dari 418000 iklan gratis di kota terdekat Anda mulai dari lowongan kerja, apartemen, hingga jodoh dan banyak lagi ✓ bebas biaya.'
+  },
+  {
+    name: 'Cari Kerja',
+    domain: 'www.carikerja.id',
+    host: 'carikerja',
+    provider: 'carikerja',
+    logo: '//cdn.urbanhire.com/img/publisher/cari-kerja.png',
+    description: 'Carikerja.id diciptakan untuk mempermudah para pencari dan pemasang lowongan pekerjaan di Indonesia.'
+  },
+  {
+    name: 'Post Job Free',
+    domain: 'www.postjobfree.com',
+    host: 'postjobfree',
+    provider: 'postjobfree',
+    logo: '//cdn.urbanhire.com/img/publisher/post-job-free.png',
+    description: 'We distribute your jobs to popular job search sites.'
+  },
+  {
+    name: 'Rulla',
+    domain: 'id.rulla.com',
+    host: 'rulla',
+    provider: 'rulla',
+    logo: '//cdn.urbanhire.com/img/publisher/rulla.png',
+    description: 'Satu-satunya situs web untuk penelusuran lowongan kerja di Indonesia.'
+  }
+]
