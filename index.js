1223cd0f187821c5a9779f9c17ea00892b2a631a
Hengki Sihombing <hengki.sihombing@gmail.com>
2017-11-02 11:49:48 +0700

    fixing load files

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 'use strict'
 
-const fs = require('fs')
-const path = require('path')
-const fastparallel = require('fastparallel')
-const files = fs.readdirSync(__dirname + '/data')
+var fs = require('fs')
+var path = require('path')
 
-fastparallel(files, (file, cb) => {
-  const extname = path.extname(file)
-  const basename = path.basename(file, extname)
+fs.readdirSync(__dirname + '/data').forEach((file) => {
+  console.log(file)
+  var extname = path.extname(file)
+  var basename = path.basename(file, extname)
 
   if (~file.indexOf('.js')) {
     exports[basename] = require(__dirname + '/data/' + file)
   }
-
-  cb()
 })
