99a8f364020fd86099353fc5a95eb16ee825e1c4
Gratcy <gratcypalma@gmail.com>
2018-01-12 19:02:41 +0700

    Field of study (#2)
    * add new helper variable field of study
    
    * reformat bk-helper same /w other
    
    * update version v2.2

---
+++ data/fieldOfStudy.js
@@ -0,0 +1,214 @@
+'use strict'
+
+module.exports = [
+   {  
+      name:'Advertising/Media'
+   },
+   {  
+      name:'Agriculture/Aquaculture/Forestry'
+   },
+   {  
+      name:'Airline Operation/Airport Management'
+   },
+   {  
+      name:'Architecture'
+   },
+   {  
+      name:'Art/Design/Creative Multimedia'
+   },
+   {  
+      name:'Biology'
+   },
+   {  
+      name:'BioTechnology'
+   },
+   {  
+      name:'Business Studies/Administration/Management'
+   },
+   {  
+      name:'Chemistry'
+   },
+   {  
+      name:'Commerce'
+   },
+   {  
+      name:'Computer Science/Information Technology'
+   },
+   {  
+      name:'Dentistry'
+   },
+   {  
+      name:'Economics'
+   },
+   {  
+      name:'Journalism'
+   },
+   {  
+      name:'Education/Teaching/Training'
+   },
+   {  
+      name:'Engineering (Aviation/Aeronautics/Astronautics)'
+   },
+   {  
+      name:'Engineering (Bioengineering/Biomedical)'
+   },
+   {  
+      name:'Engineering (Chemical)'
+   },
+   {  
+      name:'Engineering (Civil)'
+   },
+   {  
+      name:'Engineering (Computer/Telecommunication)'
+   },
+   {  
+      name:'Engineering (Electrical/Electronic)'
+   },
+   {  
+      name:'Engineering (Environmental/Health/Safety)'
+   },
+   {  
+      name:'Engineering (Industrial)'
+   },
+   {  
+      name:'Engineering (Marine)'
+   },
+   {  
+      name:'Engineering (Material Science)'
+   },
+   {  
+      name:'Engineering (Mechanical)'
+   },
+   {  
+      name:'Engineering (Mechatronic/Electromechanical)'
+   },
+   {  
+      name:'Engineering (Metal Fabrication/Tool & Die/Welding)'
+   },
+   {  
+      name:'Engineering (Mining/Mineral)'
+   },
+   {  
+      name:'Engineering (Others)'
+   },
+   {  
+      name:'Engineering (Petroleum/Oil/Gas)'
+   },
+   {  
+      name:'Finance/Accountancy/Banking'
+   },
+   {  
+      name:'Food & Beverage Services Management'
+   },
+   {  
+      name:'Food Technology/Nutrition/Dietetics'
+   },
+   {  
+      name:'Geographical Science'
+   },
+   {  
+      name:'Geology/Geophysics'
+   },
+   {  
+      name:'History'
+   },
+   {  
+      name:'Hospitality/Tourism/Hotel Management'
+   },
+   {  
+      name:'Human Resource Management'
+   },
+   {  
+      name:'Humanities/Liberal Arts'
+   },
+   {  
+      name:'Logistic/Transportation'
+   },
+   {  
+      name:'Law'
+   },
+   {  
+      name:'Library Management'
+   },
+   {  
+      name:'Linguistics/Languages'
+   },
+   {  
+      name:'Mass Communications'
+   },
+   {  
+      name:'Mathematics'
+   },
+   {  
+      name:'Medical Science'
+   },
+   {  
+      name:'Medicine'
+   },
+   {  
+      name:'Maritime Studies'
+   },
+   {  
+      name:'Marketing'
+   },
+   {  
+      name:'Music/Performing Arts Studies'
+   },
+   {  
+      name:'Nursing'
+   },
+   {  
+      name:'Optometry'
+   },
+   {  
+      name:'Personal Services'
+   },
+   {  
+      name:'Pharmacy/Pharmacology'
+   },
+   {  
+      name:'Philosophy'
+   },
+   {  
+      name:'Physical Therapy/Physiotherapy'
+   },
+   {  
+      name:'Physics'
+   },
+   {  
+      name:'Political Science'
+   },
+   {  
+      name:'Property Development/Real Estate Management'
+   },
+   {  
+      name:'Protective Services & Management'
+   },
+   {  
+      name:'Psychology'
+   },
+   {  
+      name:'Quantity Survey'
+   },
+   {  
+      name:'Science & Technology'
+   },
+   {  
+      name:'Secretarial'
+   },
+   {  
+      name:'Social Science/Sociology'
+   },
+   {  
+      name:'Sports Science & Management'
+   },
+   {  
+      name:'Textile/Fashion Design'
+   },
+   {  
+      name:'Urban Studies/Town Planning'
+   },
+   {  
+      name:'Veterinary'
+   }
+]
