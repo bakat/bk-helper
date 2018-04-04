3fd2fba7c02e78ff3f0332c9fc0f05d5323e4f12
Gratcy <gratcypalma@gmail.com>
2018-04-04 14:24:52 +0700

    cut-e variable defined

---
+++ data/cutEAssessment.js
@@ -0,0 +1,228 @@
+'use strict'
+
+module.exports = {
+    masterMind: [{
+        projectId: 153836,
+        projectName: 'Graduates',
+        instrument: [
+            {
+                instrumentId: 102,
+                name: 'Shapes (management)',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                }
+            },
+            {
+                instrumentId: 201,
+                name: 'Views',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                }
+            },
+            {
+                instrumentId: 354,
+                name: 'Scales EQL',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            },
+            {
+                instrumentId: 752,
+                name: 'Scales Verbal (Instruct)',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            },
+            {
+                instrumentId: 344,
+                name: 'Scales LST',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            },
+            {
+                instrumentId: 315,
+                name: 'Scales LT-E',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            }
+        ]
+    },
+    {
+        projectId: 153838,
+        projectName: 'Experienced Hires',
+        instrument: [
+            {
+                instrumentId: 102,
+                name: 'Shapes (management)',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                }
+            },
+            {
+                instrumentId: 201,
+                name: 'Views',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                }
+            },
+            {
+                instrumentId: 162,
+                name: 'Squares',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            },
+            {
+                instrumentId: 311,
+                name: 'Scales Numerical (Consumer)',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            },
+            {
+                instrumentId: 312,
+                name: 'Scales Verbal (Consumer)',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            },
+            {
+                instrumentId: 344,
+                name: 'Scales LST',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            },
+            {
+                instrumentId: 383,
+                name: 'Scales CLX',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            },
+            {
+                instrumentId: 315,
+                name: 'Scales LT-E',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            }
+        ]
+    },
+    {
+        projectId: 155118,
+        projectName: 'Executives/Senior Managers',
+        instrument: [
+            {
+                instrumentId: 102,
+                name: 'Shapes (management)',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                }
+            },
+            {
+                instrumentId: 201,
+                name: 'Views',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                }
+            },
+            {
+                instrumentId: 344,
+                name: 'Scales LST',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            }
+        ]
+    },
+    {
+        projectId: 255120,
+        projectName: 'Basic Role/Service Staff',
+        instrument: [
+            {
+                instrumentId: 354,
+                name: 'Scales EQL',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            },
+            {
+                instrumentId: 752,
+                name: 'Scales Verbal (Instruct)',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            },
+            {
+                instrumentId: 344,
+                name: 'Scales LST',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                }
+            }
+        ]
+    }],
+    Shapes: [
+        'Directive',
+        'Persuasive',
+        'Socially Confident',
+        'Expressive',
+        'Cooperative',
+        'Perceptive',
+        'Evaluative',
+        'Results Oriented ',
+        'Planful',
+        'Conscientious',
+        'Analytical',
+        'Conceptual',
+        'Imaginative',
+        'Change Oriented ',
+        'Autonomous',
+        'Ambitious',
+        'Competitive',
+        'Energetic'
+    ]
+}
