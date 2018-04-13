1004bdbca94016461207b4135d48274cc64ec938
Gratcy <gratcypalma@gmail.com>
2018-04-13 15:11:42 +0700

    Cut e (#4)
    * cut-e variable defined
    
    * ganti nama testnya
    
    * add flag isShapes
    
    * add description and link in Cut-E
    
    * update helper CutE

---
+++ data/cutEAssessment.js
@@ -0,0 +1,288 @@
+'use strict'
+
+module.exports = {
+    masterMind: [{
+        projectId: 153836,
+        projectName: 'Graduates',
+        instrument: [
+            {
+                instrumentId: 102,
+                name: 'Work-related Behavior',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                },
+                isShapes: true,
+                description: 'Assesses workplace behaviour',
+                link: 'https://www.cut-e.com/nc/details/shapes-management-work-related-behavior/'
+            },
+            {
+                instrumentId: 201,
+                name: 'Work-related Interests and Motives',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                },
+                isShapes: false,
+                description: 'Assesses interests and motives.',
+                link: 'https://www.cut-e.com/nc/details/views-work-related-interests-and-motives/'
+            },
+            {
+                instrumentId: 354,
+                name: 'Numeracy',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description: 'Measures numerical comprehension.',
+                link: 'https://www.cut-e.com/nc/details/scales-eql-numeracy/'
+            },
+            {
+                instrumentId: 752,
+                name: 'Understanding of basic Instructions',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description: 'Measures the ability to find and interpret written information in passages of text.',
+                link: 'https://www.cut-e.com/nc/details/scales-verbal-instruct-understanding-of-basic-instructions/'
+            },
+            {
+                instrumentId: 344,
+                name: 'Deductive - Logical thinking',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description: 'Measures deductive reasoning ability.',
+                link: 'https://www.cut-e.com/nc/details/scales-lst-deductive-logical-thinking/'
+            },
+            {
+                instrumentId: 315,
+                name: 'Language Skills - English',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description: 'Measures English proficiency.',
+                link: 'https://www.cut-e.com/nc/details/scales-lt-e-language-skills-english/'
+            }
+        ]
+    },
+    {
+        projectId: 153838,
+        projectName: 'Experienced Hires',
+        instrument: [
+            {
+                instrumentId: 102,
+                name: 'Work-related Behavior',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                },
+                isShapes: true,
+                description: 'Assesses workplace behaviour.',
+                link: 'https://www.cut-e.com/nc/details/shapes-management-work-related-behavior/'
+            },
+            {
+                instrumentId: 201,
+                name: 'Work-related Interests and Motives',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                },
+                isShapes: false,
+                description: 'Assesses interests and motives.',
+                link: 'https://www.cut-e.com/nc/details/views-work-related-interests-and-motives/'
+            },
+            {
+                instrumentId: 162,
+                name: 'Situational Behaviour',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                },
+                isShapes: false,
+                description: 'Assesses situational behaviour.',
+                link: 'https://www.cut-e.com/nc/details/squares-situational-behaviour/'
+            },
+            {
+                instrumentId: 311,
+                name: 'Numerical Reasoning',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description: 'Measures the ability to find and use numerical information shown in tables, graphs and diagrams.',
+                link: 'https://www.cut-e.com/nc/details/scales-numerical-consumer-numerical-reasoning/'
+            },
+            {
+                instrumentId: 312,
+                name: 'Verbal Reasoning',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description: 'Measures the ability to find and interpret written information in passages of text.',
+                link: 'https://www.cut-e.com/nc/details/scales-verbal-consumer-verbal-reasoning/'
+            },
+            {
+                instrumentId: 344,
+                name: 'Deductive reasoning',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description: 'Measures deductive reasoning ability.',
+                link: 'https://www.cut-e.com/nc/details/scales-lst-deductive-logical-thinking/'
+            },
+            {
+                instrumentId: 383,
+                name: 'Inductive - Logical Thinking',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description: 'Measures inductive logical reasoning.',
+                link: 'https://www.cut-e.com/nc/details/scales-cls-inductive-logical-thinking/'
+            },
+            {
+                instrumentId: 315,
+                name: 'Language skills - English',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description: 'Measures English proficiency.',
+                link: 'https://www.cut-e.com/nc/details/scales-lt-e-language-skills-english/'
+            }
+        ]
+    },
+    {
+        projectId: 155118,
+        projectName: 'Executives/Senior Managers',
+        instrument: [
+            {
+                instrumentId: 102,
+                name: 'Work-related Behavior',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                },
+                isShapes: true,
+                description: 'Assesses workplace behaviour.',
+                link: 'https://www.cut-e.com/nc/details/shapes-management-work-related-behavior/'
+            },
+            {
+                instrumentId: 201,
+                name: 'Work-related Interests and Motives',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                },
+                isShapes: false,
+                description: 'Assesses interests and motives.',
+                link: 'https://www.cut-e.com/nc/details/views-work-related-interests-and-motives/'
+            },
+            {
+                instrumentId: 162,
+                name: 'Situational Behaviour',
+                autoDisQualify: false,
+                scores: {
+                    min: 1,
+                    max: 9
+                },
+                isShapes: false,
+                description:'Assesses situational behaviour.',
+                link: 'https://www.cut-e.com/nc/details/squares-situational-behaviour/'
+            }
+        ]
+    },
+    {
+        projectId: 255120,
+        projectName: 'Basic Role/Service Staff',
+        instrument: [
+            {
+                instrumentId: 354,
+                name: 'Numeracy',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description:'Measures numerical comprehension.',
+                link: 'https://www.cut-e.com/nc/details/scales-eql-numeracy/'
+            },
+            {
+                instrumentId: 752,
+                name: 'Understanding of basic Instructions',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description:'Measures the ability to find and interpret written information in passages of text.',
+                link: 'https://www.cut-e.com/nc/details/shapes-management-work-related-behavior/'
+            },
+            {
+                instrumentId: 344,
+                name: 'Deductive reasoning',
+                autoDisQualify: true,
+                scores: {
+                    min: 1,
+                    max: 100
+                },
+                isShapes: false,
+                description: 'Measures deductive reasoning ability.',
+                link: 'https://www.cut-e.com/nc/details/scales-lst-deductive-logical-thinking/'
+            }
+        ]
+    }],
+    Shapes: [
+        'Directive',
+        'Persuasive',
+        'Socially Confident',
+        'Expressive',
+        'Cooperative',
+        'Perceptive',
+        'Evaluative',
+        'Results Oriented',
+        'Planful',
+        'Conscientious',
+        'Analytical',
+        'Conceptual',
+        'Imaginative',
+        'Change Oriented',
+        'Autonomous',
+        'Ambitious',
+        'Competitive',
+        'Energetic'
+    ]
+}
