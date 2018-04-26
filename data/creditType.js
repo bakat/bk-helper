42720c8bd891e6c084952948ac8e445fe9c178b3
Gratcy <gratcypalma@gmail.com>
2018-04-26 13:55:21 +0700

    add credit types

---
+++ data/creditType.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+    creditAction: {
+        {
+            key: 'top-up',
+            name: 'Top Up'
+        },
+        {
+            key: 'viewed',
+            name: 'Resume Search'
+        },
+        {
+            key: 'send-invitation',
+            name: 'Video Interview'
+        },
+        {
+            key: 'sent-psycometric-test',
+            name: 'Sent Psycometric Test'
+        }
+    }
+}
