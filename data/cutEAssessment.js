b508b7a85e96d01be887a514a41c8b3cd73d3d31
Gratcy <gratcypalma@gmail.com>
2018-05-11 20:50:19 +0700

    Refaktor shape (#8)
    * update competency
    
    * refactor shapes names
    
    * set competencies label
    
    * add formula for competencies
    
    * fixed rule formula competencies
    
    * change name assessmnet

--- data/cutEAssessment.js
+++ data/cutEAssessment.js
@@ -45,7 +45,7 @@ module.exports = {
     },
     {
         projectId: 153836,
-        projectName: 'Graduates',
+        projectName: 'Graduates / Staff',
         instrument: [
             {
                 instrumentId: 102,
@@ -123,7 +123,7 @@ module.exports = {
     },
     {
         projectId: 153838,
-        projectName: 'Experienced Hires',
+        projectName: 'Experienced Hires / MT / ODP',
         instrument: [
             {
                 instrumentId: 102,
@@ -225,7 +225,7 @@ module.exports = {
     },
     {
         projectId: 155118,
-        projectName: 'Executives/Senior Managers',
+        projectName: 'Executives / Senior Managers',
         instrument: [
             {
                 instrumentId: 102,
@@ -264,26 +264,245 @@ module.exports = {
                 link: 'https://www.cut-e.com/nc/details/squares-situational-behaviour/'
             }
         ]
-    },
-],
+    }],
     Shapes: [
-        'Directive',
-        'Persuasive',
-        'Socially Confident',
-        'Expressive',
-        'Cooperative',
-        'Perceptive',
-        'Evaluative',
-        'Results Oriented',
-        'Planful',
-        'Conscientious',
-        'Analytical',
-        'Conceptual',
-        'Imaginative',
-        'Change Oriented',
-        'Autonomous',
-        'Ambitious',
-        'Competitive',
-        'Energetic'
+        {
+            index: 0,
+            name: 'Directing'
+        },
+        {
+            index: 1,
+            name: 'Persuasive'
+        },
+        {
+            index: 2,
+            name: 'Socially Confident'
+        },
+        {
+            index: 3,
+            name: 'Sociable'
+        },
+        {
+            index: 4,
+            name: 'Agreeable'
+        },
+        {
+            index: 5,
+            name: 'Behavioural'
+        },
+        {
+            index: 6,
+            name: 'Prudent'
+        },
+        {
+            index: 7,
+            name: 'Focused on Results'
+        },
+        {
+            index: 8,
+            name: 'Systematic'
+        },
+        {
+            index: 9,
+            name: 'Conscientious'
+        },
+        {
+            index: 10,
+            name: 'Analytical'
+        },
+        {
+            index: 11,
+            name: 'Conceptual'
+        },
+        {
+            index: 12,
+            name: 'Imaginative'
+        },
+        {
+            index: 13,
+            name: 'Open to Change'
+        },
+        {
+            index: 14,
+            name: 'Autonomous'
+        },
+        {
+            index: 15,
+            name: 'Achieving'
+        },
+        {
+            index: 16,
+            name: 'Competitive'
+        },
+        {
+            index: 17,
+            name: 'Energetic'
+        }
+    ],
+    Competencies: [
+        {
+            index: 0,
+            name: 'Vision & Strategy'
+        },
+        {
+            index: 1,
+            name: 'Initiative & Responsibility'
+        },
+        {
+            index: 2,
+            name: 'Business Development'
+        },
+        {
+            index: 3,
+            name: 'Bottom Line Focus'
+        },
+        {
+            index: 4,
+            name: 'Influence'
+        },
+        {
+            index: 5,
+            name: 'Networking'
+        },
+        {
+            index: 6,
+            name: 'People Management'
+        },
+        {
+            index: 7,
+            name: 'People Development'
+        },
+        {
+            index: 8,
+            name: 'Organizational Awareness'
+        },
+        {
+            index: 9,
+            name: 'Execution'
+        },
+        {
+            index: 10,
+            name: 'Systematic Approach'
+        },
+        {
+            index: 11,
+            name: 'Steadiness'
+        },
+        {
+            index: 12,
+            name: 'AnalysisJudgment'
+        },
+        {
+            index: 13,
+            name: 'Professional Expertise'
+        },
+        {
+            index: 14,
+            name: 'Innovation'
+        },
+        {
+            index: 15,
+            name: 'Effective Communication'
+        },
+        {
+            index: 16,
+            name: 'Constructive Teamwork'
+        },
+        {
+            index: 17,
+            name: 'Self-development'
+        }
+    ],
+    competenciesFormula: [
+        {
+            index: 0,
+            shapes: [6, 11, 12],
+            formula: '((%s + 2 * %s + %s) / 4 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 1,
+            shapes: [7, 14, 15],
+            formula: '((%s + 2 * %s + 2 * %s) / 5 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 2,
+            shapes: [1, 15, 16, 17],
+            formula: '((%s + %s + 2 * %s + %s) / 5 * 1.4) * (2 + 5)'
+        },
+        {
+            index: 3,
+            shapes: [7, 8],
+            formula: '((2 * %s + %s) / 3 * 1.2) * (2 + 5)'
+        },
+        {
+            index: 4,
+            shapes: [1, 2],
+            formula: '((2 * %s + %s) / 3 * 1.2) * (2 + 5)'
+        },
+        {
+            index: 5,
+            shapes: [2, 3],
+            formula: '((%s + 2 * %s) / 3 * 1.2) * (2 + 5)'
+        },
+        {
+            index: 6,
+            shapes: [0, 1, 2],
+            formula: '((3 * %s + %s + 2 * %s) / 6 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 7,
+            shapes: [4, 5, 12],
+            formula: '((%s + 2 * %s + %s) / 4 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 8,
+            shapes: [3, 5, 6],
+            formula: '((2 * %s + %s + 2 * %s) / 5 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 9,
+            shapes: [7, 9, 14],
+            formula: '((%s + %s + (-2 * %s)) / 4 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 10,
+            shapes: [6, 8, 9],
+            formula: '((%s + 2 * %s + 2 * %s) / 5 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 11,
+            shapes: [2, 5, 17],
+            formula: '((%s + %s + %s) / 3 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 12,
+            shapes: [6, 9, 10],
+            formula: '((%s + %s + 3 * %s) / 5 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 13,
+            shapes: [10, 11, 15],
+            formula: '((%s + %s + 2 * %s) / 4 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 14,
+            shapes: [12, 13, 14],
+            formula: '((3 * %s + 2 * %s + %s) / 4 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 15,
+            shapes: [1, 5, 11],
+            formula: '((2 * %s + %s + 2 * %s) / 5 * 1.4) * (2 + 5)'
+        },
+        {
+            index: 16,
+            shapes: [3, 4, 16],
+            formula: '((%s + 2 * %s + (-%s)) / 4 * 1.3) * (2 + 5)'
+        },
+        {
+            index: 17,
+            shapes: [5, 13, 15],
+            formula: '((%s + %s + 2 * %s) / 4 * 1.3) * (2 + 5)'
+        }
     ]
 }
